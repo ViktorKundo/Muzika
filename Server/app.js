1a50c7ad80a367113192b1ebb00db61c1c1ba5b6
Viktor Kundovic <viktor.vico05@gmail.com>
2022-11-30 02:47:02 +0100

    Prototip Unosa Bendova | 02:46

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./db/connection");
+const bandSchema = require("./models/band_model")
 const app = express();
 
 app.use(express.json());
@@ -10,6 +11,17 @@ PORT = 5000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
+        console.log("Successfully connected to database!");
+        await new bandSchema({
+            bandUserName: "zvezdice123",
+            bandName: "Starset",
+            password: "prejaki",
+            email: "starset@gmail.com",
+            bandParticipants: [
+                {firstName: "Luka", lastName: "Bogdanovic", Role: "Pevac"},
+                {firstname: "Sveti", lastName: "Mici", Role: "Vanga"},
+                ]
+        }).save()
         app.listen(PORT, () => {
             console.log(`Server is listening on port ${PORT}.....`);
         })
--- Server/models/band_model.js
+++ Server/models/band_model.js
@@ -1,8 +1,28 @@
 const mongoose = require("mongoose");
 
+const reqString = {
+    type: String, 
+    unique: true,
+};
+
+const participantsSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    Role: String,
+});
+
+const songsSchema = new mongoose.Schema({
+    songName: String,
+    songDuration: Number,
+});
+
 const bandSchema = new mongoose.Schema({
-    bandName: {type: String, unique: true},
-    password: String,
-    email: String,
-    bandParticipants,
-})
+    bandUserName: reqString,
+    bandName: String,
+    password: reqString,
+    email: reqString,
+    bandParticipants: [participantsSchema],
+    songs: [songsSchema],
+});
+
+module.exports = mongoose.model("bands",bandSchema);
