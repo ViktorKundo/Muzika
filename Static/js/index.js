5bc6e986ba72aa6bbb14cbc26a02a035d7ad5a40
Aleksandar Temelkov <temelkovaleksa@gmail.com>
2022-11-30 02:35:15 +0100

    02:34 | Updated index.html, main.css and index.js

--- Static/js/index.js
+++ Static/js/index.js
@@ -5,3 +5,27 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     })
 })
+
+
+document.getElementById("login").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "flex";
+    disableScroll();
+})
+
+document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "none";
+    enableScroll();
+})
+
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
